83fe12c8cc77d73382df3272651376a3814e5369
haminse <haminse@naver.com>
2022-11-04 03:14:55 -0700

    thirdday

---
+++ JS_grammer/list.js
@@ -0,0 +1,23 @@
+// how to change css property in js
+document.getElementById('lion').style.color = 'red';  
+document.getElementsByTagName('li')[1].style.color = 'blue';  
+
+document.querySelectorAll(".bear")[0].style.color = "red"; // selector all => array type
+
+
+// change html string in js
+document.querySelectorAll(".bear")[0].innerHTML= "dogs";
+
+// add another tag + content with js
+const animals = document.getElementById('lion');
+animals.innerHTML += "<li class = 'animal'>cat</li>";
+
+// make new css class in object
+document.querySelectorAll(".box")[0].classList.add('purple');
+
+// remove css class in obj
+document.querySelectorAll(".box")[0].classList.remove('purple');
+
+//toggle -> if class exists ? del : add (always reverse) 
+document.querySelectorAll(".box")[0].classList.toggle("yellow");
+document.querySelectorAll(".box")[0].classList.toggle("yellow");
