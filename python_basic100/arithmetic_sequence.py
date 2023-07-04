de96ab308cddbd04916a5b290f78e58982f4b93b
haminse <haminse@naver.com>
2023-07-04 18:29:33 +0900

    daily_coding

--- python_basic100/arithmetic_sequence.py
+++ python_basic100/arithmetic_sequence.py
@@ -1,3 +1,19 @@
-a,d,n = map(int, input().split())
+# a,d,n = map(int, input().split())
 
-print(a + (n-1)*d)
+# print(a + (n-1)*d)
+
+
+
+# geometric progression
+# a,r,n = map(int, input().split())
+
+# print(a*r**(n-1))
+
+
+# sequence
+a,m,d,n = map(int, input().split())
+rst = a
+for _ in range(n-1):
+    rst = rst*m + d
+
+print(rst)
